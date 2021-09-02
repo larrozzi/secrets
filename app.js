f35b822416951d1967cec537d89b6eaf577cf825
Yassine Laaroussi <ylaaroussi@tmiclimatesolutions.com>
2021-09-02 19:44:25 -0400

    setup sign in with google

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const session = require('express-session')
 const passport = require("passport")
 const passportLocalMongoose = require("passport-local-mongoose");
 const { deserializeUser } = require('passport');
-
+//const GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate")
 
 const app = express();
 
@@ -38,20 +40,54 @@ mongoose.set("useCreateIndex", true) // to remove deprecate warning collection.e
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 })
 
 userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(findOrCreate)
 
 const User = mongoose.model("User", userSchema)
 
 passport.use(User.createStrategy()) //create a local login strategy
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/",(req,res)=>{
     res.render("home")
 })
+
+app.get("/auth/google", 
+passport.authenticate('google',{scope:['profile']})
+)
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 
 app.get("/login",(req,res)=>{
     res.render("login")
@@ -69,6 +105,18 @@ app.get("/secrets",(req,res)=>{
        res.redirect("/login")
    }
 })
+
+app.get("/submit", (req, res)=>{
+  if(req.isAuthenticated()){
+    res.render("submit")
+  }else{
+    res.redirect("/login")
+  }
+})
+
+app.get("/logout", (req, res)=>{
+    req.logout();
+}) 
 
 app.get("/logout",(req, res)=>{
     req.logout()
